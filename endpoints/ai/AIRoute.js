07588d1017d32ec34347f9d4f04f8eecc235ac8f
Paul-Ole Saalfeld <p.saalfeld@flohr-cie.de>
2024-02-08 22:36:37 +0100

    added AI Calls for PostPage

--- endpoints/ai/AIRoute.js
+++ endpoints/ai/AIRoute.js
@@ -15,12 +15,10 @@ router.post("/", isAuthenticated, function (req, res, next) {
                 if (error) {
                     logger.error("Problems parsing user input: " + error);
                     res.status(400).end();
-                }
-                if (!result || !(result.method == "GET" || result.method == "POST")) {
+                } else if (!result || !(result.method == "GET" || result.method == "POST")) {
                     logger.error("Parsing the user input resulted in no valid method");
                     res.status(400).end();
-                }
-                if (result.method == "GET" && result.data && result.data.postID) {
+                } else if (result.method == "GET" && result.data && result.data.postID) {
                     aiService.getPostPage(result.data.postID, function (error, returnObj) {
                         if (error) {
                             logger.error("Problems transforming data: " + error);
@@ -46,18 +44,13 @@ router.post("/", isAuthenticated, function (req, res, next) {
                 if (error) {
                     logger.error("Problems parsing user input: " + error);
                     res.status(400).end();
-                }
-                // TODO: Logic executing API Call
-                if (!result || !(result.entity == "post" || result.entity == "comment")) {
+                } else if (!result || !(result.entity == "post" || result.entity == "comment")) {
                     logger.error("Parsing the user input resulted in no valid entity");
                     res.status(400).end();
-                }
-                if (!(result.method == "POST" || result.method == "PUT" || result.method == "DELETE")) {
+                } else if (!(result.method == "POST" || result.method == "PUT" || result.method == "DELETE")) {
                     logger.error("Parsing the user input resulted in no valid method");
                     res.status(400).end();
-                }
-                if (result.entity == "post" && result.method == "PUT" && result.data && (result.data.title || result.data.text)) {
-                    // TODO: Send title and text back and execute another API Call to update the post
+                } else if (result.entity == "post" && result.method == "PUT" && result.data && (result.data.title || result.data.text)) {
                     aiService.updatePost(postID, result.data.title, result.data.text, function (error, returnObj) {
                         if (error) {
                             logger.error("Problems transforming data: " + error);
@@ -66,8 +59,7 @@ router.post("/", isAuthenticated, function (req, res, next) {
                         res.send(returnObj);
                     });
                 } else if (result.entity == "post" && result.method == "DELETE") {
-                    // TODO: Send postID back and execute another API Call to delete the post
-                    aiService.deletePost(postID, function (error, returnObj) {
+                    aiService.deletePost(function (error, returnObj) {
                         if (error) {
                             logger.error("Problems transforming data: " + error);
                             res.status(400).end();
@@ -75,8 +67,7 @@ router.post("/", isAuthenticated, function (req, res, next) {
                         res.send(returnObj);
                     });
                 } else if (result.entity == "comment" && result.method == "POST" && result.data && result.data.text) {
-                    // TODO: Send text back and execute another API Call to create the comment
-                    aiService.createComment(postID, result.data.text, function (error, returnObj) {
+                    aiService.createComment(result.data.text, function (error, returnObj) {
                         if (error) {
                             logger.error("Problems transforming data: " + error);
                             res.status(400).end();
@@ -84,8 +75,7 @@ router.post("/", isAuthenticated, function (req, res, next) {
                         res.send(returnObj);
                     });
                 } else if (result.entity == "comment" && result.method == "PUT" && result.data && result.data.text && result.data.commentID) {
-                    // TODO: Send text and commentID back and execute another API Call to update the comment
-                    aiService.updateComment(commentID, result.data.text, function (error, returnObj) {
+                    aiService.updateComment(result.data.commentID, result.data.text, function (error, returnObj) {
                         if (error) {
                             logger.error("Problems transforming data: " + error);
                             res.status(400).end();
@@ -93,8 +83,7 @@ router.post("/", isAuthenticated, function (req, res, next) {
                         res.send(returnObj);
                     });
                 } else if (result.entity == "comment" && result.method == "DELETE" && result.data && result.data.commentID) {
-                    // TODO: Send commentID back and execute another API Call to delete the comment
-                    aiService.deleteComment(commentID, function (error, returnObj) {
+                    aiService.deleteComment(result.data.commentID, function (error, returnObj) {
                         if (error) {
                             logger.error("Problems transforming data: " + error);
                             res.status(400).end();
--- endpoints/ai/AIService.js
+++ endpoints/ai/AIService.js
@@ -15,80 +15,18 @@ function parseInputForumPage(userInput, callback) {
         if (err) {
             logger.error("Error getting ForumPosts: " + err);
             callback(err, null);
-        }
-        const forumPostsString = forumPosts.map(obj => (JSON.stringify(obj))).join(", ");
-        logger.debug("Additional current posts info for AI: " + forumPostsString);
-        // TODO: Fine Tune Prompt for cases where not enough input is given
-        ai.chat.completions.create({
-            model: 'gpt-3.5-turbo',
-            messages: [{ "role": "system", "content": 'You are a helpful assistant converting user input into one of two json formats, which will follow later. The json data will then be used to do API calls. So never differ from the formats. Depending on if you think someone wants to open something or create something you will use one of these formats: {"method": "GET", "data": {"postID": "put the corresponding postID here"} or {"method": "POST", "data": {"title": "put the extracted or matching title here", "text": "put the text of the forumpost here"}}' },
-            { "role": "system", "content": `For GET requests you can use this list of the current Forum Posts to find out the postID: "${forumPostsString}"` },
-            { "role": "user", "content": `Parse the following user input into a matching json format to be used for an API Call: "Erstelle einen Post mit dem Titel "Mein erster Post" und dem Text "Moin Leute, für was ist dieses Forum gedacht?""` },
-            { "role": "assistant", "content": '{"method": "POST", "data": {"title": "Mein erster Post", "text": "Moin Leute, für was ist dieses Forum gedacht?"}}' },
-            { "role": "user", "content": `Parse the following user input into a matching json format to be used for an API Call (There is no post with this title, thats why the postID is assumed to be 0): "Öffne den Post mit dem Titel "Skaten in Tempelhof""` },
-            { "role": "assistant", "content": '{"method": "GET", "data": {"postID": "0"}}' },
-            { "role": "user", "content": `Parse the following user input into a matching json format to be used for an API Call: "${userInput}"` }]
-            // max_tokens: 500
-        }).then(gptResponse => {
-            const parsedDataString = gptResponse.choices[0].message.content;
-            logger.debug("Successfully parsed user input: " + parsedDataString);
-            const parsedDataJson = JSON.parse(parsedDataString);
-            callback(null, parsedDataJson);
-        }).catch(error => {
-            logger.error("Error parsing userinput in GPT: " + error);
-            callback("Error parsing userinput in GPT", null);
-        })
-    });
-}
-
-function getPostPage(postID, callback) {
-    if (isNaN(postID)) {
-        callback("The postID is not a number", null);
-    }
-    const returnObj = { call: "GET_post", body: { postID: postID } }
-    callback(null, returnObj);
-}
-
-function createPost(title, text, callback) {
-    if (title.length === 0 || text.length === 0) {
-        callback("The title or text is empty", null);
-    }
-    const returnObj = { call: "POST_post", body: { title: title, text: text } }
-    callback(null, returnObj);
-}
-
-function parseInputPostPage(userInput, postID, callback) {
-    logger.info("AIService: Parsing user Input on a Post Page");
-    forumPostService.findPostBy(postID, function (err, post) {
-        if (err) {
-            logger.error("There is no forumPost with this postID");
-            callback(err, null);
-        }
-        commentService.getCommentsFromPost(postID, function (err2, comments) {
-            if (err2) {
-                logger.error("There is no forumPost with this postID");
-                callback(err2, null);
-            }
-            // TODO: Maybe sanitize data before giving it to AI - maybe give username to AI to help with checking for "my" comment
-            const forumPostString = JSON.stringify(post);
-            logger.debug("Additional current post info for AI: " + forumPostString);
-            const commentsString = comments.map(obj => (JSON.stringify(obj))).join(", ");
-            logger.debug("Additional current comments on post info for AI: " + commentsString);
-            // TODO: Tune Prompt with post and comments Data for Post Page
+        } else {
+            const forumPostsString = forumPosts.map(obj => (JSON.stringify(obj))).join(", ");
+            logger.debug("Additional current posts info for AI: " + forumPostsString);
+            // TODO: Fine Tune Prompt for cases where not enough input is given
             ai.chat.completions.create({
                 model: 'gpt-3.5-turbo',
-                messages: [{ "role": "system", "content": 'You are a helpful assistant converting user input into one of three json formats, which will follow later. The json data will then be used to do API calls. So never differ from the formats. In the format there will be a key value pair where the key is entity and you should put either "post" or "comment" as the value depending on if you think the user input is about a forumpost or a comment. Its not possible to create a new forumpost. Depending on if you think someone wants to create, update or delete something you will use one of these formats: {"method": "POST", "entity": "comment", "data": {"text": "put the text of the comment here"}}, {"method": "PUT", "entity": "put comment or post here" "data": {"title": "put the new extracted or matching title here, leave empty for a comment entity", "text": "put the new text of the forumpost or comment here", "commentID": "for comments put the extracted commentID here, leave empty for a post entity"}}, {"method": "DELETE", "entity": "put comment or post here", "data": {"commentID": "for comments put the extracted commentID here, leave empty for a post entity"}}' },
-                { "role": "system", "content": `For PUT and DELETE requests for the entity comment you can use this list of the current comments on this post to find out the commentID: "${commentsString}"` },
-                { "role": "user", "content": `Parse the following user input into a matching json format to be used for an API Call: "Update den Post bitte zu dem neuen Titel "Mittlerweile habe ich Erfahrung" und dem Text "Achso, dafür ist das Forum gedacht!""` },
-                { "role": "assistant", "content": '{"method": "PUT", "entity": "post", "data": {"title": "Mittlerweile habe ich Erfahrung", "text": "Achso, dafür ist das Forum gedacht!"}}' },
-                { "role": "user", "content": `Parse the following user input into a matching json format to be used for an API Call: "Lösche den Post"` },
-                { "role": "assistant", "content": '{"method": "DELETE", "entity": "post"}' },
-                { "role": "user", "content": `Parse the following user input into a matching json format to be used for an API Call: "Erstelle einen Kommentar mit dem Text "Ich persönlich finde den Skatepark eigentlich recht gut!""` },
-                { "role": "assistant", "content": '{"method": "POST", "entity": "comment", "data": {"text": "Ich persönlich finde den Skatepark eigentlich recht gut!"}}' },
-                { "role": "user", "content": `Parse the following user input into a matching json format to be used for an API Call (There is no comment with this text in the data, thats why the commentID is assumed to be 0): "Update den Kommentar "Ich persönlich finde..." zu "Jetzt wo ich nochmal nachgedacht habe, bin ich mir doch nicht mehr so sicher.""` },
-                { "role": "assistant", "content": '{"method": "PUT", "entity": "comment", "data": {"text": "Jetzt wo ich nochmal nachgedacht habe, bin ich mir doch nicht mehr so sicher.", "commentID": "0"}}' },
-                { "role": "user", "content": `Parse the following user input into a matching json format to be used for an API Call (There is no comment with this text in the data, thats why the commentID is assumed to be 0): "Lösche den Kommentar mit dem Text "Jetzt wo ich nochmal nachgedacht habe, bin ich mir doch nicht mehr so sicher.""` },
-                { "role": "assistant", "content": '{"method": "DELETE", "entity": "comment", "data": {"commentID": "0"}}' },
+                messages: [{ "role": "system", "content": 'You are a helpful assistant converting user input into one of two json formats, which will follow later. The json data will then be used to do API calls. So never differ from the formats. Depending on if you think someone wants to open something or create something you will use one of these formats: {"method": "GET", "data": {"postID": "put the corresponding postID here"} or {"method": "POST", "data": {"title": "put the extracted or matching title here", "text": "put the text of the forumpost here"}}' },
+                { "role": "system", "content": `For GET requests you can use this list of the current Forum Posts to find out the postID: "${forumPostsString}"` },
+                { "role": "user", "content": `Parse the following user input into a matching json format to be used for an API Call: "Erstelle einen Post mit dem Titel "Mein erster Post" und dem Text "Moin Leute, für was ist dieses Forum gedacht?""` },
+                { "role": "assistant", "content": '{"method": "POST", "data": {"title": "Mein erster Post", "text": "Moin Leute, für was ist dieses Forum gedacht?"}}' },
+                { "role": "user", "content": `Parse the following user input into a matching json format to be used for an API Call (There is no post with this title, thats why the postID is assumed to be 0): "Öffne den Post mit dem Titel "Skaten in Tempelhof""` },
+                { "role": "assistant", "content": '{"method": "GET", "data": {"postID": "0"}}' },
                 { "role": "user", "content": `Parse the following user input into a matching json format to be used for an API Call: "${userInput}"` }]
                 // max_tokens: 500
             }).then(gptResponse => {
@@ -97,36 +35,137 @@ function parseInputPostPage(userInput, postID, callback) {
                 const parsedDataJson = JSON.parse(parsedDataString);
                 callback(null, parsedDataJson);
             }).catch(error => {
-                logger.error("Error parsing userinput in GPT: " + error);
-                callback("Error parsing userinput in GPT", null);
+                callback(error, null);
             })
-        });
+        }
+    });
+}
+
+function getPostPage(postID, callback) {
+    if (isNaN(postID)) {
+        callback("The postID is not a number", null);
+    } else {
+        const returnObj = { call: "GET_post", body: { postID: postID } }
+        callback(null, returnObj);
+    }
+}
+
+function createPost(title, text, callback) {
+    if (title.length === 0 || text.length === 0) {
+        callback("The title or text is empty", null);
+    } else {
+        const returnObj = { call: "POST_post", body: { title: title, text: text } }
+        callback(null, returnObj);
+    }
+}
+
+// returns {method: "POST | PUT | DELETE", data: {title: "", text: "", commentID: ""}} to callback function
+function parseInputPostPage(userInput, postID, callback) {
+    logger.info("AIService: Parsing user Input on a Post Page");
+    forumPostService.findPostBy(postID, function (err, post) {
+        if (err) {
+            logger.error("There is no forumPost with this postID");
+            callback(err, null);
+        } else {
+            commentService.getCommentsFromPost(postID, function (err2, comments) {
+                if (err2) {
+                    logger.error("There is no forumPost with this postID");
+                    callback(err2, null);
+                } else {
+                    // TODO: Maybe sanitize data before giving it to AI - maybe give username to AI to help with checking for "my" comment
+                    const forumPostString = JSON.stringify(post);
+                    logger.debug("Additional current post info for AI: " + forumPostString);
+                    const commentsString = comments.map(obj => (JSON.stringify(obj))).join(", ");
+                    logger.debug("Additional current comments on post info for AI: " + commentsString);
+                    // TODO: Tune Prompt with post and comments Data for Post Page
+                    ai.chat.completions.create({
+                        model: 'gpt-3.5-turbo',
+                        messages: [{ "role": "system", "content": 'You are a helpful assistant converting user input into one of three json formats, which will follow later. The json data will then be used to do API calls. So never differ from the formats. In the format there will be a key value pair where the key is entity and you should put either "post" or "comment" as the value depending on if you think the user input is about a forumpost or a comment. Its not possible to create a new forumpost. Depending on if you think someone wants to create, update or delete something you will use one of these formats: {"method": "POST", "entity": "comment", "data": {"text": "put the text of the comment here"}}, {"method": "PUT", "entity": "put comment or post here" "data": {"title": "put the new extracted or matching title here, leave empty for a comment entity", "text": "put the new text of the forumpost or comment here", "commentID": "for comments put the extracted commentID here, leave empty for a post entity"}}, {"method": "DELETE", "entity": "put comment or post here", "data": {"commentID": "for comments put the extracted commentID here, leave empty for a post entity"}}' },
+                        { "role": "system", "content": `For PUT and DELETE requests for the entity comment you can use this list of the current comments on this post to find out the commentID: "${commentsString}"` },
+                        { "role": "user", "content": `Parse the following user input into a matching json format to be used for an API Call: "Update den Post bitte zu dem neuen Titel "Mittlerweile habe ich Erfahrung" und dem Text "Achso, dafür ist das Forum gedacht!""` },
+                        { "role": "assistant", "content": '{"method": "PUT", "entity": "post", "data": {"title": "Mittlerweile habe ich Erfahrung", "text": "Achso, dafür ist das Forum gedacht!"}}' },
+                        { "role": "user", "content": `Parse the following user input into a matching json format to be used for an API Call: "Lösche den Post"` },
+                        { "role": "assistant", "content": '{"method": "DELETE", "entity": "post"}' },
+                        { "role": "user", "content": `Parse the following user input into a matching json format to be used for an API Call: "Erstelle einen Kommentar mit dem Text "Ich persönlich finde den Skatepark eigentlich recht gut!""` },
+                        { "role": "assistant", "content": '{"method": "POST", "entity": "comment", "data": {"text": "Ich persönlich finde den Skatepark eigentlich recht gut!"}}' },
+                        { "role": "user", "content": `Parse the following user input into a matching json format to be used for an API Call (There is no comment with this text in the data, thats why the commentID is assumed to be 0): "Update den Kommentar "Ich persönlich finde..." zu "Jetzt wo ich nochmal nachgedacht habe, bin ich mir doch nicht mehr so sicher.""` },
+                        { "role": "assistant", "content": '{"method": "PUT", "entity": "comment", "data": {"text": "Jetzt wo ich nochmal nachgedacht habe, bin ich mir doch nicht mehr so sicher.", "commentID": "0"}}' },
+                        { "role": "user", "content": `Parse the following user input into a matching json format to be used for an API Call (There is no comment with this text in the data, thats why the commentID is assumed to be 0): "Lösche den Kommentar mit dem Text "Jetzt wo ich nochmal nachgedacht habe, bin ich mir doch nicht mehr so sicher.""` },
+                        { "role": "assistant", "content": '{"method": "DELETE", "entity": "comment", "data": {"commentID": "0"}}' },
+                        { "role": "user", "content": `Parse the following user input into a matching json format to be used for an API Call: "${userInput}"` }]
+                        // max_tokens: 500
+                    }).then(gptResponse => {
+                        const parsedDataString = gptResponse.choices[0].message.content;
+                        logger.debug("Successfully parsed user input: " + parsedDataString);
+                        const parsedDataJson = JSON.parse(parsedDataString);
+                        callback(null, parsedDataJson);
+                    }).catch(error => {
+                        callback(error, null);
+                    });
+                }
+            });
+        }
     });
 }
 
 function updatePost(postID, title, text, callback) {
-    const returnObj = {}
+    if ((title === undefined || title.length === 0) && (text === undefined || text.length === 0)) {
+        callback("The title and the text is empty", null);
+    } else if (!(title === undefined || title.length === 0) && !(text === undefined || text.length === 0)) {
+        const returnObj = { call: "PUT_post", body: { title: title, text: text } }
+        callback(null, returnObj);
+    } else {
+        forumPostService.findPostBy(postID, function (err, post) {
+            if (err) {
+                logger.error("There is no forumPost with this postID");
+                callback(err, null);
+            } else {
+                if (title.length > 0) {
+                    const oldPostText = post.content;
+                    const returnObj = { call: "PUT_post", body: { title: title, text: oldPostText } }
+                    callback(null, returnObj);
+                } else {
+                    const oldPostTitle = post.titel;
+                    const returnObj = { call: "PUT_post", body: { title: oldPostTitle, text: text } }
+                    callback(null, returnObj);
+                }
+            }
+        });
+    }
+}
+
+function deletePost(callback) {
+    const returnObj = { call: "DELETE_post" }
     callback(null, returnObj);
 }
 
-function deletePost(postID, callback) {
-    const returnObj = {}
-    callback(null, returnObj);
-}
-
-function createComment(postID, text, callback) {
-    const returnObj = {}
-    callback(null, returnObj);
+function createComment(text, callback) {
+    if (text.length === 0) {
+        callback("The text is empty", null);
+    } else {
+        const returnObj = { call: "POST_comment", body: { text: text } }
+        callback(null, returnObj);
+    }
 }
 
 function updateComment(commentID, text, callback) {
-    const returnObj = {}
-    callback(null, returnObj);
+    if (isNaN(commentID)) {
+        callback("The commentID is not a number", null);
+    } else if (text.length === 0) {
+        callback("The text is empty", null);
+    } else {
+        const returnObj = { call: "PUT_comment", body: { text: text, commentID: commentID } }
+        callback(null, returnObj);
+    }
 }
 
-function deleteComment(commentID, text, callback) {
-    const returnObj = {}
-    callback(null, returnObj);
+function deleteComment(commentID, callback) {
+    if (isNaN(commentID)) {
+        callback("The commentID is not a number", null);
+    } else {
+        const returnObj = { call: "DELETE_comment", body: { commentID: commentID } }
+        callback(null, returnObj);
+    }
 }
 
 module.exports = {
